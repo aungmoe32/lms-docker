63dc7fa439cd6c37a716e6adab0f44c946146fe6
Aung Moe Myint Thu <aungmoemyintthu@gmail.com>
2024-01-04 00:21:01 +0630

    update force https v2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,7 +29,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         if (env('APP_ENV') == 'production') {
-            $url->forceScheme('https');
+            // $url->forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
